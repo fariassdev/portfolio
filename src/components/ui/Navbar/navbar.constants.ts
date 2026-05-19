9f149ea943b1be7e111316979d47ce7deb6ab7b6
Fernando Arias <fernan.fpinformatica@gmail.com>
2026-05-19 02:13:43 +0200

    refactor: rename work section in navbar to projects

--- src/components/ui/Navbar/navbar.constants.ts
+++ src/components/ui/Navbar/navbar.constants.ts
@@ -1,6 +1,6 @@
 export const navItems = [
   { label: '// HOME', href: '#home' },
-  { label: '// WORK', href: '#projects' },
+  { label: '// PROJECTS', href: '#projects' },
   { label: '// STACK', href: '#stack' },
   { label: '// ABOUT', href: '#experience' },
   { label: '// CONTACT', href: '#contact' },
